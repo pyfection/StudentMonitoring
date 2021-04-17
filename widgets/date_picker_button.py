e70bb9a21a0a60abe2fd7f0d895b9450257ed5f2
Matthias Schreiber <mat@pyfection.com>
2021-04-17 12:34:54 -0400

    Closes #43 - Datepicker choose year shows old years; greatly improved speed of date picker

--- widgets/date_picker_button.py
+++ widgets/date_picker_button.py
@@ -1,6 +1,7 @@
+import datetime
 
 from kivy.lang.builder import Builder
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty
 from kivymd.uix.button import MDRoundFlatIconButton
 from kivymd.uix.picker import MDDatePicker
 
@@ -11,16 +12,28 @@ Builder.load_file('widgets/date_picker_button.kv')
 class DatePickerButton(MDRoundFlatIconButton):
     date = StringProperty()
     format = StringProperty('%d-%m-%Y')
+    date_dialog = MDDatePicker()
+    min_date = ObjectProperty()
+    min_year = NumericProperty(1914)
+    max_year = datetime.date.today().year
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.size_hint_x = None
         self.width = 400
+        self.date_dialog.bind(on_save=self.on_save)
 
     def show_date_picker(self):
-        date_dialog = MDDatePicker()
-        date_dialog.bind(on_save=self.on_save)
-        date_dialog.open()
+        self.date_dialog.open()
 
     def on_save(self, instance, value, date_range):
         self.date = value.strftime("%Y-%m-%d")
+
+    def on_min_date(self, inst, min_date):
+        self.date_dialog.min_date = min_date
+
+    def on_min_year(self, inst, min_year):
+        self.date_dialog.min_year = min_year
+
+    def on_max_year(self, inst, max_year):
+        self.date_dialog.max_year = max_year
