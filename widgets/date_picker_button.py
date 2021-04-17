82474e5cfb1a6d2a46fbca2e3949798e6b67c6dc
Matthias Schreiber <mat@pyfection.com>
2021-04-17 13:34:11 -0400

    removed min and max dates and just kept years for date picker buttons

--- widgets/date_picker_button.py
+++ widgets/date_picker_button.py
@@ -13,7 +13,6 @@ class DatePickerButton(MDRoundFlatIconButton):
     date = StringProperty()
     format = StringProperty('%d-%m-%Y')
     date_dialog = MDDatePicker()
-    min_date = ObjectProperty()
     min_year = NumericProperty(1914)
     max_year = datetime.date.today().year
 
@@ -24,16 +23,9 @@ class DatePickerButton(MDRoundFlatIconButton):
         self.date_dialog.bind(on_save=self.on_save)
 
     def show_date_picker(self):
+        self.date_dialog.min_year = self.min_year
+        self.date_dialog.max_year = self.max_year
         self.date_dialog.open()
 
     def on_save(self, instance, value, date_range):
         self.date = value.strftime("%Y-%m-%d")
-
-    def on_min_date(self, inst, min_date):
-        self.date_dialog.min_date = min_date
-
-    def on_min_year(self, inst, min_year):
-        self.date_dialog.min_year = min_year
-
-    def on_max_year(self, inst, max_year):
-        self.date_dialog.max_year = max_year
